882d61e7485881ba303988e3ff1e628b3b07467b
Tim Ker <tim.ker@gmail.com>
2013-08-13 21:19:49 +0100

    tidy

--- Extension/contentscript.js
+++ Extension/contentscript.js
@@ -1,126 +1,78 @@
 
-//alert("ahhh");
-//var xhr = new XMLHttpRequest();
-//xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
-//xhr.open("GET", chrome.extension.getURL('/data.json'), true);
-//xhr.send()
 
+ (function() {
 
-// todo check dom element doesn't exist
-  // get template, or show/hide
+    $('#AT_Manuel_Root').remove();
+     // this is to disable double loading
+     // this could be used for a "close" button
+     // if(document.getElementById('AT.Manuel.Root'))
+     // {
+     //   $(document.getElementById('AT.Manuel.Root')).remove();
+     //   return;
+     // }
 
- (function(){
-
-  $('#AT_Manuel_Root').remove();
-     //if(document.getElementById('AT.Manuel.Root'))
-     //{
-      //$(document.getElementById('AT.Manuel.Root')).remove();
+    $.get(chrome.extension.getURL('/template.html'), function(data) {
+        $("body").after(data);
+        //safer loading
+        //could find the first ref to ng-app or put at bottom, to in AT.Manuel.Root 
       
-     // return;
-     //}
+        // Or if you're using jQuery 1.8+:
+        // this might still work if we convert to element first, instead of text?
+        // $($.parseHTML(data)).appendTo('body');
 
-  $.get(chrome.extension.getURL('/template.html'), function(data) {
-   
-     // $(data).appendTo('body');
-      $("body").after(data);
-
-      //safer loading
-      //could find the first ref to ng-app or put at bottom, to in AT.Manuel.Root 
-      
-
-      // Or if you're using jQuery 1.8+:
-      // $($.parseHTML(data)).appendTo('body');
-
-     var first = document.getElementById('AT_Manuel_Root');
-
-
-    	angular.element(first).ready(function() {
-     // alert("moo");
-    console.log(first);
-
-  //chrome.storage.local.get  
-          //angular.bootstrap(document);
+        var first = document.getElementById('AT_Manuel_Root');
+        angular.element(first).ready(function() {
+          //  console.log(first);
           angular.bootstrap(first);
-
-  //also should remove head/ etc etc
-
-  // this might still work if we convert to element first, instead of text?
-        // angular.bootstrap( data);
-         });
-
-  });
+        });
+    });
 })();
 
-//$.ajax({
-//  url: chrome.extension.getURL('/template.html')
-
-//}).done(function ( data ) {
-//  console.log("data");
-//  console.log(data);
-//});
-
-var lastItem = function()
-{
-  var lastElementFocused;
-
-  
-
-  var setValue = function(text)
-  {
-    alert("c");
-  }
-
-}
 
 
-var testModule = (function () {
+var pageAccessor = (function () {
   var lastElementFocused;
   
-   init = function() {
-      // todo this needs to be "live or on"
-      // todo does not seem to cycle through ne elements
-      $("*").on('blur', function(args) {
+  init = function() {
+    // todo this needs to be "live or on"
+    // todo does not seem to cycle through ne elements
+    $("*").on('blur', function(args) {
       //$( "*" ).blur(function(args) {
       // console.log("lost focus");
-         lastElementFocused = args.currentTarget;
-      });
+      lastElementFocused = args.currentTarget;
+    });
 
-    };
+  };
 
-    nextItem = function(){
-               //should onlt be txt inputs,textarea
-               var allInputs = $("input,select");
-            console.log(allInputs);
-            for (var i = 0; i < allInputs.length; i++) {
-                if (allInputs[i] == lastElementFocused) {
-                  // todo I think this might be for checkboxes
-                  // while ((allInputs[i]).name == (allInputs[i + 1]).name) {
-                  //    i++;
-                  //}
-console.log("found");
-console.log(i + 1);
-console.log(allInputs.length);
-                    if ((i + 1) < allInputs.length) {
-                       $(allInputs[i + 1]).focus();
-                     } else {
-                       $(allInputs[0]).focus();
-                     }
-                }
-            }
+  nextItem = function(){
+    //should onlt be txt inputs,textarea
+    var allInputs = $("input,select");
+    console.log(allInputs);
+    for (var i = 0; i < allInputs.length; i++) {
+      if (allInputs[i] == lastElementFocused) {
+        // todo I think this might be for checkboxes
+        // while ((allInputs[i]).name == (allInputs[i + 1]).name) {
+        //    i++;
+        // }
+        console.log("found");
+        console.log(i + 1);
+        console.log(allInputs.length);
+        if ((i + 1) < allInputs.length) {
+           $(allInputs[i + 1]).focus();
+        } else {
+           $(allInputs[0]).focus();
+        }
+      }
+    }
+  };
 
+  init();
 
-    };
-
-init();
   return {
-
     setValue: function (value) {
       $(lastElementFocused).val(value);
       nextItem();
-    //alert(value);
     },
-
-    
   };
 
 })();
@@ -128,43 +80,6 @@ init();
 
 
 
-//this should only run once
-//todo  also make this a live/on event
-//only needs to be inputs?
-
-
-
-
-function getURLInfo(url,callback)
-{
-    var xhr = new XMLHttpRequest();
-    xhr.open
-        (
-            "GET",
-            url,
-            true
-        );
-    xhr.send();
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState == 4)
-        {
-        	console.log(xhr.responseText);
-        	callback(xhr.responseText);
-            //return JSON.parse(xhr.responseText);
-           // console.log(JSON.parse(xhr.responseText));
-        }
-    }
-}
-
-
-//getURLInfo(chrome.extension.getURL('/data.json'));
-
-
-//getURLInfo(chrome.extension.getURL('/template.html'), function(data){
-//console.log(data);
-
-//});
 
 
 
@@ -193,13 +108,7 @@ function TodoCtrl($scope)
 
 $scope.ApplyItemToTextField = function(todo)
   {
-      //  console.log($(":active"));
-    //console.log(document.activeElement);
-    //$(document.activeElement).val(todo.text);
-    //alert();
-//lastItem.setValue(todo.text);
-testModule.setValue(todo.text);
-//next elelment focus
+  pageAccessor.setValue(todo.text);
 
   }
 
