e18138cb2e3c2d5912de1c6a2c714ea326244f0e
Tim Ker <tim.ker@gmail.com>
2013-09-04 08:10:10 +0100

    added clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,42 @@
 module.exports = function(grunt) {
 
+//  var yeomanConfig = {
+ //       app: 'app',
+  //      dist: 'dist'
+  //  };
+
+    require('time-grunt')(grunt);
 	grunt.loadNpmTasks('grunt-contrib-copy');
- 
+ grunt.loadNpmTasks('grunt-contrib-clean');
+ //todo
+//add version
+//add zip
+
 	grunt.initConfig({
-	copy: {
-  main: {
-    files: [
-      {src: ['Extension/*'], dest: 'dist/', filter: 'isFile'}, // includes files in path
-      {src: ['Extension/**'], dest: 'dist/'}, // includes files in path and its subdirs
-      {expand: true, cwd: 'Extension/', src: ['**'], dest: 'dist/'}, // makes all src relative to cwd
-      {expand: true, flatten: true, src: ['Extension/**'], dest: 'dist/', filter: 'isFile'} // flattens results to a single level
-    ]
-  }
-}
+
+		clean: ["dist"],
+		copy: {
+		  main: {
+		    files: [
+		      {src: ['Extension/*'], dest: 'dist/', filter: 'isFile'}, // includes files in path
+		      {src: ['Extension/**'], dest: 'dist/'}, // includes files in path and its subdirs
+		      {expand: true, cwd: 'Extension/', src: ['**'], dest: 'dist/'}, // makes all src relative to cwd
+		      {expand: true, flatten: true, src: ['Extension/**'], dest: 'dist/', filter: 'isFile'} // flattens results to a single level
+		    ]
+		  }
+		}
 
  	});
-  //grunt.registerTask('default', []);
 
  // A very basic default task.
   grunt.registerTask('logit', 'Log some stuff.', function() {
     grunt.log.write('Logging some stuff...').ok();
   });
 
-  grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...').ok();
-  });
 
-grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...2').ok();
-  });
 
-grunt.registerTask('default', ["copy"]);
+grunt.registerTask('default', ["logit","clean","copy"]);
+
+
 
 };
